37a9bebfdb6989b4086afbaf52ac9b2d73d4c7ca
Agnieszka Łyczak <agnieszka.lyczak02@gmail.com>
2025-03-09 17:27:14 +0100

    Funkcjonalność Karty Produktu; kosmetyczne poprawki

---
+++ Data/Models/CreateProductCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RitaAppBlazor.Data.Models
+{
+	public class CreateProductCard
+	{
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+		[JsonPropertyName("name")]
+		public string Name { get; set; } = "";
+		[JsonPropertyName("unitId")]
+		public int UnitId { get; set; } = 0;
+		[JsonPropertyName("categoryIds")]
+		public List<int>? CategoryIds { get; set; } = new List<int>();
+	}
+}
